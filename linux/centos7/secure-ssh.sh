7b99b86d0ff848768f5b59a32b5224a3b12d6c50
Benjamin-Whitcomb <benjamin.whitcomb@mymail.champlain.edu>
2019-02-27 11:21:03 -0500

    updated script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,10 +3,15 @@
 #Creates new ssh user called $1
 #Adds a public key to that users authorized key file
 #Disables root ssh
-
+	
 sudo useradd $1
 sudo mkdir /home/$1/.ssh
-sudo cp linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+sudo cp ~/sys265-scripts/linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys
 sudo chmod 700 /home/$1/.ssh
 sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+sudo chmod 777 /etc/ssh/sshd_config
+sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+sudo chmod 700 /etc/ssh/sshd_config
+sudo systemctl restart sshd
 
